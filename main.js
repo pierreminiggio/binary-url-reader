093e0574a84f393886a41f503ae3e73e1d18f1a4
Pierre MINIGGIO <=>
2020-01-06 13:10:16 +0100

    avoid await

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 class BinaryUrlReader
 {
-	async read(url, callback)
+	read(url, callback)
 	{
-		let blob = await fetch(url).then(r => r.blob())
-		let reader = new FileReader()
-		reader.addEventListener('load', function () {
-			callback(reader)
-		}, false);
-		reader.readAsDataURL(blob)
+		fetch(url).then(r => {
+			let reader = new FileReader()
+			reader.addEventListener('load', function () {
+				callback(reader)
+			}, false)
+			reader.readAsDataURL(r.blob())
+		})
+		
 	}
 }
 
